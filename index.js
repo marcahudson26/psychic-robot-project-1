f67579d5ddc0a5c7ef48b6ceb881ebd8ac3e1607
marc hudson <marchudson2601@gmail.com>
2023-02-02 15:38:09 +0000

    all of the working apis and paths

--- index.js
+++ index.js
@@ -1,3 +1,56 @@
+let userInput = "sushi"
+
+// this gets the ingredients
+fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${userInput}`)
+    .then(response => response.json())
+    .then(response => {
+
+        if (!response.meals) {
+            // if there is no data throw an error
+            throw "No Data";
+        }
+        const meal = response.meals[0]
+        const ingredients = []
+        const measurements = []
+
+        for (const key in meal) {
+            //because the api response doesn't  contain an array of ingredients we loop thought the keys to create an array
+            if (key.startsWith("strIngredient")) {
+                if (meal[key] !== "") {
+                    ingredients.push(meal[key].trim())
+                }
+            }
+            if (key.startsWith("strMeasure")) {
+                if (meal[key] !== "") {
+                    measurements.push(meal[key].trim())
+                }
+            }
+        }
+        const recipeIngredents = []
+        for (let i = 0; i < ingredients.length; i++) {
+            const ingredent = ingredients[i];
+            const measurement = measurements[i]
+            recipeIngredents.push(`${measurement} ${ingredent}`)
+        }
+        //for the meal name
+        console.log(response.meals[0].strMeal)
+        //this gets the ingredents
+        console.log(recipeIngredents)
+        //cooking instructions
+        console.log(response.meals[0].strInstructions)
+        //for the the thumbnail image
+        console.log(response.meals[0].strMealThumb)
+        // for the u tube video link
+        console.log(response.meals[0].strYoutube)
+        console.log(ingredients)
+    })
+
+    .catch(() => {
+        // if there is an error render some error message
+        console.log("There was an error")
+    })
+
+
 userInput = "burger"
 
 const options = {
@@ -7,7 +60,7 @@ const options = {
         'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
     }
 };
-
+//fetch request with loop to pull put the ingredients and push into the ingredients array
 fetch(`https://edamam-recipe-search.p.rapidapi.com/search?q=${userInput}`, options)
     .then(response => response.json())
     .then(response => {
@@ -21,7 +74,7 @@ fetch(`https://edamam-recipe-search.p.rapidapi.com/search?q=${userInput}`, optio
         // ingredients
         console.log(ingredients)
         // calorie content
-        console.log(response.hits[3].recipe.calories)
+        console.log(Math.trunc(response.hits[3].recipe.calories))
         // // fat content an nutrience
         console.log(response.hits[3].recipe.digest)
         // health lables
@@ -34,4 +87,25 @@ fetch(`https://edamam-recipe-search.p.rapidapi.com/search?q=${userInput}`, optio
 
 
 
+// const userInputt = "clowne"
 
+// const apiKey = `85ab5ccbe5924069b86a34a443887846`
+// //
+// function getLocation(place) {
+//     return fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${place}&type=city&format=json&apiKey=${apiKey}`)
+//         .then(response => response.json())
+//         .then(result => result.results[0].place_id);
+// }
+
+// function getRestaurants(placeId) {
+//     return fetch(`https://api.geoapify.com/v2/places?categories=catering.restaurant&filter=place:${placeId}&limit=20&apiKey=${apiKey}`)
+//         .then(response => response.json())
+//         .then(result => result)
+// }
+
+// getLocation(userInputt).then(placeId => {
+//     getRestaurants(placeId).then(restaurants => {
+//         console.log(restaurants)
+//     })
+// })
+// .features.map(x => x.properties.name).filter(x => x)
