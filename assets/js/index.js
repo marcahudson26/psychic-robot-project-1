0ef5879c752d43a2cd2982eab3a079107bc79a44
Tobi Olajide <tobiolajide887@yahoo.com>
2023-02-02 20:41:31 +0000

    updated css and js links in html and divided js code

--- assets/js/index.js
+++ assets/js/index.js
@@ -84,28 +84,3 @@ fetch(`https://edamam-recipe-search.p.rapidapi.com/search?q=${userInput}`, optio
         //link to cooking istructions
         console.log(response.hits[3].recipe.url)
     })
-
-
-
-// const userInputt = "clowne"
-
-// const apiKey = `85ab5ccbe5924069b86a34a443887846`
-// //
-// function getLocation(place) {
-//     return fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${place}&type=city&format=json&apiKey=${apiKey}`)
-//         .then(response => response.json())
-//         .then(result => result.results[0].place_id);
-// }
-
-// function getRestaurants(placeId) {
-//     return fetch(`https://api.geoapify.com/v2/places?categories=catering.restaurant&filter=place:${placeId}&limit=20&apiKey=${apiKey}`)
-//         .then(response => response.json())
-//         .then(result => result)
-// }
-
-// getLocation(userInputt).then(placeId => {
-//     getRestaurants(placeId).then(restaurants => {
-//         console.log(restaurants)
-//     })
-// })
-// .features.map(x => x.properties.name).filter(x => x)
