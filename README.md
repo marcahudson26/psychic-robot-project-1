fa7118630e9262dbc46428ade45e9d556b729941
Matthew <mbrooks9622@gmail.com>
2023-02-03 14:41:00 +0000

    read me user story

--- README.md
+++ README.md
@@ -1,15 +1,24 @@
-# ideas from the team ------ enter below 
+# Psychic-Robot-Project-1
 
+## Our Group Task
 
-# requirements for the project
+Our Group Project task is to build a web application that uses at least two server-side APIs, is styled with a CSS framework like Bootstrap, is deployed on GitHub Pages, is interactive with modals instead of alerts, uses client-side storage for persistent data, is responsive, has a polished user interface, has a clean code repository with good coding standards, and has a quality README with a unique name, description, technologies used, screenshot, and link to the deployed application.
 
-Use Bootstrap or some other CSS framework.
-Be deployed to GitHub Pages.
-Be interactive (i.e., accept and respond to user input).
-Use at least two server-side APIs.
-Does not use alerts, confirms, or prompts (use modals).
-Use client-side storage to store persistent data.
-Be responsive.
-Have a polished UI.
-Have a clean repository that meets quality coding standards (file structure, naming conventions, follows best practices for class/id naming conventions, indentation, quality comments, etc.).
-Have a quality README (with unique name, description, technologies used, screenshot, and link to deployed application).
+## User Story:
+•	As a foodie, I want to be able to choose between eating in or eating out.
+•	Eating In - I want to the information to cook my own meals with a selection of:
+o	food item names, 
+o	descriptions, 
+o	recipes, 
+o	calories, 
+o	nutrients, 
+o	instructions and 
+o	video links, so I can enjoy my meal in the comfort of my own home.
+
+•	Eating Out – I want to be able to order from my favourite restaurant using my own postcode or city name in the search box that will provide me with restaurant’s:
+o	Location on the Map
+o	City Name
+o	Restaurant’s Name, Address and Postcode
+o	Restaurant Menu and Website URL
+So, I can enjoy dining out with friends and family.
+This user defines our target audience because the problem they are trying to solve based on the “IF” they Eat in or Eat Out they expect a set of desired outcomes. This read me is provide our development team with more context and helps to prioritise features that are most important for the user.
--- index.js
+++ index.js
@@ -1,5 +1,103 @@
+
+//https://myprojects.geoapify.com/api/R4CTeUxR4WWoY5obR5Uv/keys
+//My API Key = bd2e43573bab4fdc918ab1746b55dece
+
+
+
+
+
+
+
+async function getData() {
+    const response1 = await fetch("https://api1.com/data");
+    const data1 = await response1.json();
+  
+    const response2 = await fetch("https://api2.com/data");
+    const data2 = await response2.json();
+  
+    // Combine data
+    const combinedData = [...data1, ...data2];
+  
+    return combinedData;
+}
+
+//...........................
 // fetch()
 //     .then(response => response.json())
 //     .then(data => console.log(data))
+// My API Key: 0DdhhzRimqQBkSOJDjt2yrAjIBnunBRXkUZZ6TTz
 
-console.log("hello")
+// fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+//https://www.figma.com/team_invite/redeem/KzwIA4CDMD0VFJvmCKhbxJ
+
+// fetch("https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&pageSize=2&api_key=0DdhhzRimqQBkSOJDjt2yrAjIBnunBRXkUZZ6TTz")
+
+// .then(response => response.json())
+// .then(data => console.log(data))
+
+// console.log("hello world")
+
+//
+
+fetch("https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&pageSize=2&api_key=0DdhhzRimqQBkSOJDjt2yrAjIBnunBRXkUZZ6TTz")
+
+.then(response => response.json())
+.then(data => console.log(data))
+
+console.log("hello world")
+
+
+
+
+// let userInput = "burger"
+// // this gets the ingredients
+// fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${userInput}`)
+//     .then(response => response.json())
+//     .then(response => {
+//         console.log(response)
+//         if (!response.meals) {
+//             // if there is no data throw an error
+//             throw "No Data";
+//         }
+//         const meal = response.meals[0]
+//         const ingredients = []
+//         const measurements = []
+//         for (const key in meal) {
+//             //because the api response doesn't  contain an array of ingredients we loop thought the keys to create an array
+//             if (key.startsWith("strIngredient")) {
+//                 if (meal[key] !== "") {
+//                     ingredients.push(meal[key].trim())
+//                 }
+//             }
+//             if (key.startsWith("strMeasure")) {
+//                 if (meal[key] !== "") {
+//                     measurements.push(meal[key].trim())
+//                 }
+//             }
+//         }
+//         const recipeIngredents = []
+//         for (let i = 0; i < ingredients.length; i++) {
+//             const ingredent = ingredients[i];
+//             const measurement = measurements[i]
+//             recipeIngredents.push(`${measurement} ${ingredent}`)
+//         }
+//         //for the meal name
+//         console.log(response.meals[0].strMeal)
+//         //this gets the ingredents
+//         console.log(recipeIngredents)
+//         //cooking instructions
+//         console.log(response.meals[0].strInstructions)
+//         //for the the thumbnail image
+//         console.log(response.meals[0].strMealThumb)
+//         // for the u tube video link
+//         console.log(response.meals[0].strYoutube)
+//     })
+//     .catch(() => {
+//         // if there is an error render some error message
+//         console.log("There was an error")
+//     })
+// // this is a random meal gen for the meal
+// // fetch(`http://www.themealdb.com/api/json/v1/1/random.php`)
+// //     .then(response => response.json())
+// //     .then(response => console.log())
+
