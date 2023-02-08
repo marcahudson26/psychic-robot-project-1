70841965adc091ddc17171a8e82812cffc7b8ad2
Matthew <mbrooks9622@gmail.com>
2023-02-08 19:53:51 +0000

    updated history page to fit group work figma

--- assets/js/history.js
+++ assets/js/history.js
@@ -1,4 +1,12 @@
 
+// blur screen on nav-bar click
+$('.dropdown').on('show.bs.dropdown', function () {
+    document.querySelector(".overlay").classList.remove("d-none")
+})
+$('.dropdown').on('hide.bs.dropdown', function () {
+    document.querySelector(".overlay").classList.add("d-none")
+})
+
 // let userInput = "sushi"
 
 // // this gets the ingredients
