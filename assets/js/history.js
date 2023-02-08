b1ec56d1c82f50bfe79717224a81691ec969027a
Matthew <mbrooks9622@gmail.com>
2023-02-08 21:01:21 +0000

    displaying fav restaurant buttons to history page

--- assets/js/history.js
+++ assets/js/history.js
@@ -1,4 +1,34 @@
 
+//this is selecting a container where my "fav restuarants" are going to
+let favRestaurantsDiv = document.querySelector("#favRestaurants");
+
+//we are retrieving from the local storage and storing it into
+let storedFavRestaurants = JSON.parse(localStorage.getItem("favRestaurants"));
+
+//We console what we want to see
+console.log(storedFavRestaurants);
+displayRestaurant();
+function displayRestaurant () {
+    // function to display fav restaurants on the page
+    for (let i = 0; i < storedFavRestaurants.length; i++) {
+        // getting the restaurant name 
+        const restaurant = storedFavRestaurants[i];
+        // create/set content/append
+        let restaurantDiv = document.createElement("div")
+        restaurantDiv.innerHTML = `
+                                    <button class="restaurant-btn">${restaurant}</button>
+                                    `
+        favRestaurantsDiv.append(restaurantDiv);
+    }
+    
+}
+
+
+
+
+
+
+
 // blur screen on nav-bar click
 $('.dropdown').on('show.bs.dropdown', function () {
     document.querySelector(".overlay").classList.remove("d-none")
@@ -62,34 +92,34 @@ $('.dropdown').on('hide.bs.dropdown', function () {
 
 // userInput = "burger"
 
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': 'bda18aceb6msh4cfb8ec1d5bdafep1699e7jsnf33daa16b16d',
-        'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
-    }
-};
-//fetch request with loop to pull put the ingredients and push into the ingredients array
-fetch(`https://edamam-recipe-search.p.rapidapi.com/search?q=${userInput}`, options)
-    .then(response => response.json())
-    .then(response => {
-        ingredients = []
-        const path = response.hits[3].recipe.ingredientLines
-        for (let i = 0; i < path.length; i++) {
-            ingredients.push(path[i]);
-        }
-        // meal name
-        console.log(response.q)
-        // ingredients
-        console.log(ingredients)
-        // calorie content
-        console.log(Math.trunc(response.hits[3].recipe.calories))
-        // // fat content an nutrience
-        console.log(response.hits[3].recipe.digest)
-        // health lables
-        console.log(response.hits[3].recipe.healthLabels)
-        //image
-        console.log(response.hits[3].recipe.image)
-        //link to cooking istructions
-        console.log(response.hits[3].recipe.url)
-    })
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         'X-RapidAPI-Key': 'bda18aceb6msh4cfb8ec1d5bdafep1699e7jsnf33daa16b16d',
+//         'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
+//     }
+// };
+// //fetch request with loop to pull put the ingredients and push into the ingredients array
+// fetch(`https://edamam-recipe-search.p.rapidapi.com/search?q=${userInput}`, options)
+//     .then(response => response.json())
+//     .then(response => {
+//         ingredients = []
+//         const path = response.hits[3].recipe.ingredientLines
+//         for (let i = 0; i < path.length; i++) {
+//             ingredients.push(path[i]);
+//         }
+//         // meal name
+//         console.log(response.q)
+//         // ingredients
+//         console.log(ingredients)
+//         // calorie content
+//         console.log(Math.trunc(response.hits[3].recipe.calories))
+//         // // fat content an nutrience
+//         console.log(response.hits[3].recipe.digest)
+//         // health lables
+//         console.log(response.hits[3].recipe.healthLabels)
+//         //image
+//         console.log(response.hits[3].recipe.image)
+//         //link to cooking istructions
+//         console.log(response.hits[3].recipe.url)
+//     })
